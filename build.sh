ca2e88684686b9227bade1f7f4284693b6863542
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2021-10-29 11:08:23 +0200

    update LLVM build steps as it moved from SVN to Git

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 if [ ! -d llvm-src ]; then
   mkdir -p llvm-src
   mkdir -p llvm-obj
-  svn checkout https://llvm.org/svn/llvm-project/llvm/trunk@229311 llvm-src
-  ln -s $(pwd)/src llvm-src/projects/mila
+  git clone git@github.com:llvm/llvm-project.git llvm-src
+  cd llvm-src
+  git checkout 00bd79d794b1
+  cd ..
+  ln -s $(pwd)/src llvm-src/llvm/projects/mila
 fi
 
 cd llvm-obj
-../llvm-src/configure --enable-optimized
+../llvm-src/llvm/configure --enable-optimized
 make -j5
